60ed6c998a35be1d8126e4a40e72bed5bd47f3c2
Josef Bacik <josef@toxicpanda.com>
2020-02-12 10:51:38 -0500

    inject-error: allow for multiple triggers
    Sometimes we want a certain path for the error to occur in, so allow the
    user to specify multiple -t options to accomplish this behavior.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- inject-error.py
+++ inject-error.py
@@ -13,15 +13,21 @@ BPF_HASH(fail_pids, u64);
 int trigger_function(struct pt_regs *ctx)
 {
     u64 pid = bpf_get_current_pid_tgid();
-    u64 val = 1;
-    fail_pids.update(&pid, &val);
+    u64 zero = 0;
+    u64 *val;
+    val = fail_pids.lookup_or_init(&pid, &zero);
+    lock_xadd(val, 1);
     return 0;
 }
 
 int trigger_function_ret(struct pt_regs *ctx)
 {
     u64 pid = bpf_get_current_pid_tgid();
-    fail_pids.delete(&pid);
+    u64 *val;
+    val = fail_pids.lookup(&pid);
+    if (!val)
+        return 0;
+    lock_xadd(val, -1);
     return 0;
 }
 
@@ -32,6 +38,8 @@ int override_function(struct pt_regs *ctx)
 
     val = fail_pids.lookup(&pid);
     if (!val)
+        return 0;
+    if (*val != FAIL_CNT)
         return 0;
 
     bpf_trace_printk("overrding something\\n");
@@ -44,7 +52,7 @@ int override_function(struct pt_regs *ctx)
 parser = argparse.ArgumentParser()
 parser.add_argument("-o", "--override", help="The function to override")
 parser.add_argument("-r", "--retval", type=int, help="The return value to use")
-parser.add_argument("-t", "--trigger",
+parser.add_argument("-t", "--trigger", action='append',
     help="The function that must be called to trigger the error injection")
 parser.add_argument("-d", "--delay", type=int,
     help="The delay to wait before injecting the error")
@@ -65,6 +73,7 @@ if args.retval:
     retval = args.retval
 
 bpf_text = bpf_text.replace("RCVAL", str(retval))
+bpf_text = bpf_text.replace("FAIL_CNT", str(len(args.trigger)))
 
 print("Running command")
 p = Popen(args.COMMAND)
@@ -78,8 +87,10 @@ b = BPF(text=bpf_text)
 # Load the kretprobe first, because we want the delete guy to be in place before
 # the add guy is in place, otherwise we could error out pids that are no longer
 # in our path and cause unfortunate things to happen.
-b.attach_kretprobe(event=args.trigger, fn_name="trigger_function_ret")
-b.attach_kprobe(event=args.trigger, fn_name="trigger_function")
+for t in args.trigger:
+    b.attach_kretprobe(event=t, fn_name="trigger_function_ret")
+for t in args.trigger:
+    b.attach_kprobe(event=t, fn_name="trigger_function")
 b.attach_kprobe(event=args.override, fn_name="override_function")
 
 print("Dropping caches")
@@ -104,6 +115,8 @@ while p.poll() is None:
 # We have to remove in this order otherwise we could end up with a half
 # populated hasmap and overrding legitimate things.
 b.detach_kprobe(args.override)
-b.detach_kprobe(args.trigger)
-b.detach_kretprobe(args.trigger)
+for t in args.trigger:
+    b.detach_kprobe(t)
+for t in args.trigger:
+    b.detach_kretprobe(t)
 print("Exiting")
